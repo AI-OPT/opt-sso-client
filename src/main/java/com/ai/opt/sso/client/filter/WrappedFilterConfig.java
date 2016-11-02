20bb3ad66f38195a844c21e9ec60756253d10180
gucl <gucl@asiainfo.com>
2016-11-03 00:14:04 +0800

    解决高并发下的sso票据验证问题

--- src/main/java/com/ai/opt/sso/client/filter/WrappedFilterConfig.java
+++ src/main/java/com/ai/opt/sso/client/filter/WrappedFilterConfig.java
@@ -5,7 +5,6 @@ import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletContext;
@@ -16,24 +15,23 @@ import org.slf4j.LoggerFactory;
 
 public class WrappedFilterConfig implements FilterConfig {
 	private static final Logger LOG = LoggerFactory.getLogger(WrappedFilterConfig.class);
-	private static Map<String, String> params=new ConcurrentHashMap<String, String>();
+	private ThreadLocal<Map<String, String>> params=new ThreadLocal<Map<String, String>>();
 	
 	private FilterConfig filterConfig;
 	
-	//private static Map<String, String> map = new ConcurrentHashMap<String, String>();
 	
 	public WrappedFilterConfig(FilterConfig filterConfig){
 		this.filterConfig = filterConfig;
-		initParams();
+		//initParams();
 	}
 	/**
 	 * 根据内外网自动调整IP参数
 	 * @param currentFilterConfig
 	 * @param httpRequest
 	 */
-	public WrappedFilterConfig(FilterConfig currentFilterConfig, HttpServletRequest httpRequest) {
+	public WrappedFilterConfig(FilterConfig currentFilterConfig, ThreadLocal<Map<String, String>> params) {
 		this.filterConfig = currentFilterConfig;
-		initParams(httpRequest);
+		this.params=params;
 	}
 
 	@Override
@@ -47,12 +45,12 @@ public class WrappedFilterConfig implements FilterConfig {
 		if(value!=null){
 			return value;
 		}
-		return params.get(key);
+		return params.get().get(key);
 	}
 
 	@Override
 	public Enumeration<String> getInitParameterNames() {
-		final Iterator<String> iterator = params.keySet().iterator();
+		final Iterator<String> iterator = params.get().keySet().iterator();
 		return new Enumeration<String>() {
 
 			@Override
@@ -75,18 +73,18 @@ public class WrappedFilterConfig implements FilterConfig {
 	private void initParams(HttpServletRequest httpRequest){
 		String serverName=httpRequest.getServerName();
 		boolean innerFlag=IPHelper.isInnerIP(serverName,SSOClientUtil.getInnerDomains());
-		initParams();
+		//initParams();
 		try {
 			if(innerFlag){
 				//若是内网访问，则单点登录走内网
-				Iterator iter = params.entrySet().iterator();
+				Iterator iter = params.get().entrySet().iterator();
 					while (iter.hasNext()) {
 						Map.Entry<String, String> entry = (Map.Entry<String, String>) iter.next();
 						String key = entry.getKey();
 						if(null!=key&&!"".equals(key)&&key.endsWith("_Inner")){
 							String val = entry.getValue();
 							String keyNormal=key.replace("_Inner", "");
-							params.put(keyNormal, val);
+							params.get().put(keyNormal, val);
 						}
 						
 						
@@ -99,7 +97,7 @@ public class WrappedFilterConfig implements FilterConfig {
 	
 	private void initParams(){
 		//jvm里如果有map，则直接返回
-		if(!params.isEmpty()){
+		if(!params.get().isEmpty()){
 			return;
 		}
 		//jvm里如果没有map，则读取sso.properties文件
@@ -107,7 +105,7 @@ public class WrappedFilterConfig implements FilterConfig {
 			//同步加锁
 			synchronized (WrappedFilterConfig.class) {
 				//加锁后，还没有的话，则读取sso.properties文件，否则说明其他线程已加载，无需重复加载
-				if(params.isEmpty()){
+				if(params.get().isEmpty()){
 					Properties properties = new Properties();		
 					try {
 						ClassLoader loader = WrappedFilterConfig.class.getClassLoader();
@@ -115,7 +113,7 @@ public class WrappedFilterConfig implements FilterConfig {
 						for (Object obj : properties.keySet()) {
 							String key = (String) obj;
 							if(key!=null){
-								params.put(key.trim(), properties.getProperty(key).trim());
+								params.get().put(key.trim(), properties.getProperty(key).trim());
 							}
 						}
 					} catch (IOException e) {
@@ -130,7 +128,7 @@ public class WrappedFilterConfig implements FilterConfig {
 	}
 	
 	private void printParams(){
-		Iterator iter = this.params.entrySet().iterator();
+		Iterator iter = this.params.get().entrySet().iterator();
 			while (iter.hasNext()) {
 				Map.Entry<String, String> entry = (Map.Entry<String, String>) iter.next();
 				String key = entry.getKey();
